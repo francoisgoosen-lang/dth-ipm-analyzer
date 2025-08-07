94cc04c04ed4d7cfe670e1ab4900d40c4b6f4bd1
francoisgoosen-lang <francoisgoosen@mincon.com>
2025-08-07 09:39:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "API running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
